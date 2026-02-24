2018671de38b0fdbf3a3295bc52271b9010869e8
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-24 15:21:58 +0600

    adding slidder and swiperjs and run the slider

--- header.php
+++ header.php
@@ -9,6 +9,11 @@
     <!-- Linked favicon -->
     <link rel="shortcut icon" href="./assets/logo/logo.png" type="image/x-icon">
 
+    <!-- Linked swiper js stylesheet  -->
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
+
     <!-- Linked my custom stylesheet  -->
     <link rel="stylesheet" href="./styles/slider.css">
 </head>
@@ -16,16 +21,72 @@
 <body>
     <!-- section:: main -->
     <main>
-        <section class="slider-container">
-            <div class="slider-wrapper">
+        <section class="slider-container swiper">
+            <div class="slider-wrapper swiper-wrapper">
+
                 <!-- 1st:: slide item  -->
-                <div class="slider-item">
-                    <div class="slider-content">
+                <div class="slider-item swiper-slide">
+                    <div class="slider-content container-width">
                         <h3 class="slider-subtitle">ST. Paul Coptic Medical Team</h3>
-                        <h2 class="slider-title">PMK Hospital & Diagnostic Center</h2>
+                        <h2 class="slider-title font-playfair">PMK Hospital & Diagnostic Center</h2>
                         <p class="slider-description">PMK Hospital, an initiative of Palli Mongal Karmosuchi (PMK), delivers advanced, ethical, and community-driven healthcare—combining medical excellence with a mission to serve, uplift, and strengthen every life we touch.</p>
                         <!-- slider buttons  -->
-                        <a href="" class="slider-button"><span>Be Part of the Change</span></a>
+                        <a href="" class="slider-button"><span>Join Our Mission</span></a>
+                    </div>
+                </div>
+
+                <!-- 2nd:: slide item  -->
+                <div class="slider-item swiper-slide">
+                    <div class="slider-content container-width">
+                        <h3 class="slider-subtitle">ST. Paul Coptic Medical Team</h3>
+                        <h2 class="slider-title font-playfair">PMK Hospital & Diagnostic Center</h2>
+                        <p class="slider-description">PMK Hospital, an initiative of Palli Mongal Karmosuchi (PMK), delivers advanced, ethical, and community-driven healthcare—combining medical excellence with a mission to serve, uplift, and strengthen every life we touch.</p>
+                        <!-- slider buttons  -->
+                        <a href="" class="slider-button"><span>Join Our Mission</span></a>
+                    </div>
+                </div>
+
+                <!-- 3rd:: slide item  -->
+                <div class="slider-item swiper-slide">
+                    <div class="slider-content container-width">
+                        <h3 class="slider-subtitle">ST. Paul Coptic Medical Team</h3>
+                        <h2 class="slider-title font-playfair">PMK Hospital & Diagnostic Center</h2>
+                        <p class="slider-description">PMK Hospital, an initiative of Palli Mongal Karmosuchi (PMK), delivers advanced, ethical, and community-driven healthcare—combining medical excellence with a mission to serve, uplift, and strengthen every life we touch.</p>
+                        <!-- slider buttons  -->
+                        <a href="" class="slider-button"><span>Join Our Mission</span></a>
+                    </div>
+                </div>
+
+                <!-- 4th:: slide item  -->
+                <div class="slider-item swiper-slide">
+                    <div class="slider-content container-width">
+                        <h3 class="slider-subtitle">ST. Paul Coptic Medical Team</h3>
+                        <h2 class="slider-title font-playfair">PMK Hospital & Diagnostic Center</h2>
+                        <p class="slider-description">PMK Hospital, an initiative of Palli Mongal Karmosuchi (PMK), delivers advanced, ethical, and community-driven healthcare—combining medical excellence with a mission to serve, uplift, and strengthen every life we touch.</p>
+                        <!-- slider buttons  -->
+                        <a href="" class="slider-button"><span>Join Our Mission</span></a>
+                    </div>
+                </div>
+
+                <!-- 5th:: slide item  -->
+                <div class="slider-item swiper-slide">
+                    <div class="slider-content container-width">
+                        <h3 class="slider-subtitle">ST. Paul Coptic Medical Team</h3>
+                        <h2 class="slider-title font-playfair">PMK Hospital & Diagnostic Center</h2>
+                        <p class="slider-description">PMK Hospital, an initiative of Palli Mongal Karmosuchi (PMK), delivers advanced, ethical, and community-driven healthcare—combining medical excellence with a mission to serve, uplift, and strengthen every life we touch.</p>
+                        <!-- slider buttons  -->
+                        <a href="" class="slider-button"><span>Join Our Mission</span></a>
+                    </div>
+                </div>
+
+                <!-- 6th:: slide item  -->
+                <div class="slider-item swiper-slide">
+                    <div class="slider-content container-width">
+                        <h3 class="slider-subtitle">ST. Paul Coptic Medical Team</h3>
+                        <h2 class="slider-title font-playfair">PMK Hospital & Diagnostic Center</h2>
+                        <p class="slider-description">PMK Hospital, an initiative of Palli Mongal Karmosuchi (PMK), delivers advanced, ethical, and community-driven healthcare—combining medical excellence with a mission to serve, uplift, and strengthen every life we touch.</p>
+                        <!-- slider buttons  -->
+                        <a href="" class="slider-button"><span>Join Our Mission</span></a>
                     </div>
                 </div>
             </div>
@@ -35,6 +96,12 @@
 
     <!-- Linked font awesome script  -->
     <script src="https://kit.fontawesome.com/ff87b718c4.js" crossorigin="anonymous"></script>
+
+    <!-- Linked swiper js script -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+    <!-- Linked my custom script  -->
+    <script src="./js/slider.js"></script>
 </body>
 
 </html>
---
+++ js/slider.js
@@ -0,0 +1,6 @@
+const swiper = new Swiper(".slider-container", {
+  effect: "fade",
+  autoplay: {
+    delay: 4000,
+  },
+});
