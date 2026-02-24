71126c86e6157965cbe9dbd31da8e145bc78f2bf
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-24 13:11:30 +0600

    adding basic file folders and links and setup the structure of the slider section in header page

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PMK ! Home</title>
+
+    <!-- Linked favicon -->
+    <link rel="shortcut icon" href="./assets/logo/logo.png" type="image/x-icon">
+
+    <!-- Linked my custom stylesheet  -->
+    <link rel="stylesheet" href="./styles/slider.css">
+</head>
+
+<body>
+    <!-- section:: main -->
+    <main>
+        <section class="slider-container">
+            <div class="slider-wrapper">
+                <!-- 1st:: slide item  -->
+                <div class="slider-item">
+                    <div class="slider-content">
+                        <h3 class="slider-subtitle">ST. Paul Coptic Medical Team</h3>
+                        <h2 class="slider-title">PMK Hospital & Diagnostic Center</h2>
+                        <p class="slider-description">PMK Hospital, an initiative of Palli Mongal Karmosuchi (PMK), delivers advanced, ethical, and community-driven healthcare—combining medical excellence with a mission to serve, uplift, and strengthen every life we touch.</p>
+                        <!-- slider buttons  -->
+                        <a href="" class="slider-button"><span>Be Part of the Change</span></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+    <!-- Linked font awesome script  -->
+    <script src="https://kit.fontawesome.com/ff87b718c4.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PMK Home</title>
+    <link rel="shortcut icon" href="./assets/logo/logo.png" type="image/x-icon">
+</head>
+
+<body>
+    <!-- section:: header  -->
+    <header>
+        <h1>Welcome to PMK</h1>
+    </header>
+
+    <!-- section:: main  -->
+    <main></main>
+
+    <!-- section:: footer  -->
+    <footer></footer>
+</body>
+
+</html>
