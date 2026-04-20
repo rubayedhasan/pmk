bf8bcaa927616fa8aae21c9d44a6f00f73be1c71
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-04-20 10:58:43 +0600

    adding pmk partner section wuith responsive styles

--- partner.php
+++ partner.php
@@ -19,39 +19,42 @@
                         <span>National Accreditation</span>
                     </h3>
 
-                    <div class="partner-container">
+                    <!-- partner image container  -->
+                    <div class="accreditation-container">
                         <!-- 1st image  -->
-                        <figure>
-                            <a href="https://dss.gov.bd/">
+                        <a title="DSS" href="https://dss.gov.bd/">
+                            <figure>
                                 <img src="./assets/partners/national acc/dss.png" alt="dss">
-                            </a>
-                        </figure>
+                            </figure>
+                        </a>
 
                         <!-- 2nd image  -->
-                        <figure>
-                            <a href="https://ngoab.gov.bd/">
+                        <a title="NGO Bureau" href="https://ngoab.gov.bd/">
+                            <figure>
                                 <img src="./assets/partners/national acc/ngo-bureau.png" alt="ngo-bureau">
-                            </a>
-                        </figure>
+                            </figure>
+                        </a>
 
                         <!-- 3rd image  -->
-                        <figure>
-                            <a href="https://mra.gov.bd/">
+                        <a title="MRA" href="https://mra.gov.bd/">
+                            <figure>
                                 <img src="./assets/partners/national acc/mra.png" alt="mra">
-                            </a>
-                        </figure>
-                        <!-- 1st image  -->
-                        <figure>
-                            <a href="https://nbr.portal.gov.bd/">
+                            </figure>
+                        </a>
+
+                        <!-- 4th  image  -->
+                        <a title="NBR" href="https://nbr.portal.gov.bd/">
+                            <figure>
                                 <img src="./assets/partners/national acc/nbr.png" alt="nbr">
-                            </a>
-                        </figure>
-                        <!-- 1st image  -->
-                        <figure>
-                            <a href="https://www.bb.org.bd/en/index.php">
+                            </figure>
+                        </a>
+
+                        <!-- 5th image  -->
+                        <a title="Bangladesh Bank" href="https://www.bb.org.bd/en/index.php">
+                            <figure>
                                 <img src="./assets/partners/national acc/bangladesh_bank.png" alt="bangladesh bank">
-                            </a>
-                        </figure>
+                            </figure>
+                        </a>
                     </div>
                 </div>
                 <div class="development-partners">
@@ -59,6 +62,142 @@
                         <i class="fa-solid fa-landmark"></i>
                         <span>Development Partners</span>
                     </h3>
+
+                    <!-- partner image container  -->
+                    <div class="partner-container">
+                        <!-- 1st image  -->
+                        <a title="PKSF" href="https://pksf.org.bd/">
+                            <figure>
+                                <img src="./assets/partners/development partners/PKSF.webp" alt="pksf">
+                            </figure>
+                        </a>
+
+                        <!-- 2nd image  -->
+                        <a title="The World Bank" href="https://www.worldbank.org/ext/en/home">
+                            <figure>
+                                <img src="./assets/partners/development partners/world_bank.png" alt="world bank">
+                            </figure>
+                        </a>
+
+                        <!-- 3rd image  -->
+                        <a title="bKash" href="https://www.bkash.com/products-services/add-money?gad_source=1&gad_campaignid=21439289226&gbraid=0AAAAA9iiHy6oyzqq5ETj2N744IRoniOwv&gclid=Cj0KCQjw-pHPBhCdARIsAHXYWP_yy53o9ygzz0TzyIX-iF8beTbov0WDD7rFlwaWWLnm3w6aXUgXUJoaAqCfEALw_wcB">
+                            <figure>
+                                <img src="./assets/partners/development partners/bkash.png" alt="bkash">
+                            </figure>
+                        </a>
+
+                        <!-- 4th image  -->
+                        <a title="City Bank" href="https://www.citybankplc.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/city_bank.png" alt="City Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 5th image  -->
+                        <a title="Prime Bank" href="https://www.primebank.com.bd/">
+                            <figure>
+                                <img src="./assets/partners/development partners/prime_bank.png" alt="Prime Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 6th image  -->
+                        <a title="Uttara Bank" href="https://www.uttarabank-bd.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/uttra_bank.png" alt="Uttara Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 7th image  -->
+                        <a title="eastern bank plc" href="https://www.ebl.com.bd/">
+                            <figure>
+                                <img src="./assets/partners/development partners/eastern_bank.png" alt="eastern bank plc">
+                            </figure>
+                        </a>
+
+                        <!-- 8th image  -->
+                        <a title="midland bank plc" href="https://www.midlandbankbd.net/">
+                            <figure>
+                                <img src="./assets/partners/development partners/midlank_bank.png" alt="midland bank plc">
+                            </figure>
+                        </a>
+
+                        <!-- 9th image  -->
+                        <a title="Brac Bank" href="https://www.bracbank.com/en">
+                            <figure>
+                                <img src="./assets/partners/development partners/brac_bank.png" alt="Brac Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 10th image  -->
+                        <a title="UCB Bank" href="https://www.ucb.com.bd/">
+                            <figure>
+                                <img src="./assets/partners/development partners/ucb_bank.png" alt="UCB bank">
+                            </figure>
+                        </a>
+
+                        <!-- 11th image  -->
+                        <a title="Bank Asia" href="https://www.bankasia-bd.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/bank_asia.png" alt="Bank Asia">
+                            </figure>
+                        </a>
+
+                        <!-- 12th image  -->
+                        <a title="SBAC Bank" href="https://www.sbacbank.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/sbac_bank.png" alt="SBAC bank">
+                            </figure>
+                        </a>
+
+                        <!-- 13th image  -->
+                        <a title="Mutual Trust Bank PLC" href="https://www.mutualtrustbank.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/mutual_trust_bank.png" alt="Mutual Trust Bank PLC">
+                            </figure>
+                        </a>
+
+                        <!-- 14th image  -->
+                        <a title="One Bank" href="https://www.onebank.com.bd/">
+                            <figure>
+                                <img src="./assets/partners/development partners/one_bank.png" alt="One Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 15th image  -->
+                        <a title="NRBC Bank" href="https://www.nrbcommercialbank.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/nrbc_bank.png" alt="NRBC Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 16th image  -->
+                        <a title="Pubali Bank" href="https://www.pubalibangla.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/pubali_bank.png" alt="Pubali Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 17th image  -->
+                        <a title="Jamuna Bank" href="https://jamunabankbd.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/jamuna_bank.png" alt="Jamuna Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 18th image  -->
+                        <a title="IPDC Bank" href="https://ipdc.com/">
+                            <figure>
+                                <img src="./assets/partners/development partners/ipdc_bank.png" alt="IPDC Bank">
+                            </figure>
+                        </a>
+
+                        <!-- 19th image  -->
+                        <a title="NCC Bank" href="https://www.nccbank.com.bd/">
+                            <figure>
+                                <img src="./assets/partners/development partners/ncc_bank.png" alt="NCC Bank">
+                            </figure>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
