138848e956fcb0c529ccc47c5a3da51da0f093bc
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-06-08 14:57:14 +0600

    adding map on the contact page

--- pages/contact.php
+++ pages/contact.php
@@ -46,11 +46,19 @@
                 </p>
 
                 <div class="contact-button-container">
-                    <a href="tel:+8801709914000" class="contact-button">
+                    <a href="tel:+8801709914000" class="contact-button" style="color:var(--pmk-white); background-color: var(--pmk-green); margin-right:8px;">
                         <span>
                             <i class="fa-solid fa-square-phone"></i>
                         </span>
-                        <span>Call Now</span></a>
+                        <span>Call Now</span>
+                    </a>
+
+                    <a href="mailto:info@pmk-bd.org" class="contact-button">
+                        <span>
+                            <i class="fa-solid fa-envelope"></i>
+                        </span>
+                        <span>Email Us</span>
+                    </a>
                 </div>
             </div>
         </section>
@@ -59,9 +67,35 @@
         <section id="contact-Info">
             <div class="container-width">
                 <div class="hq-info">
-                    <h3 class="hq-label">Headquarters</h3>
-                    <p class="hq-place">Mamata Palli</p>
-                    <p class="hq-location">Zirabo, Ashulia, Dhaka-1341, Bangladesh</p>
+                    <div class="hq-info-container">
+                        <div class="location-icon">
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-map-pin">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6" />
+                            </svg>
+                        </div>
+                        <div>
+                            <h3 class="hq-label">Headquarters</h3>
+                            <p class="hq-place">Mamata Palli</p>
+                            <p class="hq-location">Zirabo, Ashulia, Dhaka-1341, Bangladesh</p>
+                        </div>
+                    </div>
+                    <div class="map-button-container">
+                        <a href="#maps" class="view-map">
+                            <span>
+                                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-map-pin-2">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M12 18.5l-3 -1.5l-6 3v-13l6 -3l6 3l6 -3v7" />
+                                    <path d="M9 4v13" />
+                                    <path d="M15 7v5" />
+                                    <path d="M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879" />
+                                    <path d="M19 18v.01" />
+                                </svg>
+                            </span>
+
+                            <span>View On Map</span>
+                        </a>
+                    </div>
                 </div>
 
                 <div class="info-cards">
@@ -158,9 +192,107 @@
         <section id="maps">
             <div class="container-width">
                 <div class="maps-header">
-                    <span>Our Location</span>
-                    <h3 class="maps-label">Visit PMK Office</h3>
+                    <span class="maps-tag">Our Location</span>
+                    <h3 class="maps-label">Visit <span>PMK</span> Office</h3>
                     <p class="maps-description">Find PMK’s location and connect with us directly to support community programs and social impact initiatives.</p>
+                </div>
+
+                <!-- office map layout  -->
+                <div class="map-layout">
+                    <!-- head office  -->
+                    <aside class="map-head-office">
+                        <div class="map-card">
+                            <div class="map-header">
+                                <div class="mh-icon">
+                                    <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-map-pin">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6" />
+                                    </svg>
+                                </div>
+                                <div class="mh-content">
+                                    <h4 class="mh-title">Headquarters — Mamata Palli</h4>
+                                    <p class="mh-text">Zirabo, Ashulia, Dhaka-1341, Bangladesh</p>
+                                </div>
+                            </div>
+
+                            <!-- map frame  -->
+                            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=13nVwgFN0pMXQivQlL2CeC7wM3VNrCng&ehbc=2E312F" class="map-frame"></iframe>
+
+                            <div class="map-footer">
+                                <div class="map-meta">
+                                    <span>
+                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clock">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                                            <path d="M12 7v5l3 3" />
+                                        </svg>
+                                    </span>
+
+                                    <span>Sun–Thu: 09AM – 05PM</span>
+                                </div>
+
+                                <a href="https://www.google.com/maps/d/u/0/embed?mid=13nVwgFN0pMXQivQlL2CeC7wM3VNrCng&ehbc=2E312F" target="_blank" class="map-direct-link">
+                                    <span>Get Directions</span>
+                                    <span>
+                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+                                            <path d="M11 13l9 -9" />
+                                            <path d="M15 4h5v5" />
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </aside>
+
+                    <!-- all offices  -->
+                    <aside class="map-branch-offices">
+                        <div class="map-card">
+                            <div class="map-header">
+                                <div class="mh-icon">
+                                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-map-pin">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                        <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0" />
+                                    </svg>
+                                </div>
+                                <div class="mh-content">
+                                    <h4 class="mh-title">Our Branch Offices</h4>
+                                    <p class="mh-text">Explore PMK's branch offices across Bangladesh</p>
+                                </div>
+                            </div>
+
+                            <!-- map frame  -->
+                            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1zJSzOndzsXUYqjFYL87LglkUcIfY3bA&ehbc=2E312F" class="map-frame"></iframe>
+
+                            <div class="map-footer">
+                                <div class="map-meta">
+                                    <span>
+                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clock">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                                            <path d="M12 7v5l3 3" />
+                                        </svg>
+                                    </span>
+
+                                    <span>Sun–Thu: 09AM – 05PM</span>
+                                </div>
+
+                                <a href="https://www.google.com/maps/d/u/0/embed?mid=1zJSzOndzsXUYqjFYL87LglkUcIfY3bA&ehbc=2E312F" target="_blank" class="map-direct-link">
+                                    <span>Get Directions</span>
+                                    <span>
+                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+                                            <path d="M11 13l9 -9" />
+                                            <path d="M15 4h5v5" />
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </aside>
                 </div>
             </div>
         </section>
