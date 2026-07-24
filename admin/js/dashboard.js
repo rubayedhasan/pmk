45aa3e2fa947f44b491bbeb87685ead6a83e150f
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-07-24 16:33:22 +0600

    write dashboar menu collapsed functionality

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -4,3 +4,22 @@ const dMenuLinks = document.querySelectorAll(".d-menu-link");
 dMenuLinks.forEach((link) => {
   link.classList.toggle("d-active", link.getAttribute("href") === pageLocation);
 });
+
+// dashboard menu controller functionality
+let isCollapsed = false;
+const dMenuController = document.querySelector(".d-menu-controller");
+const dMenuContainer = document.querySelector("#dashboard-menu-container");
+const dContentContainer = document.querySelector(
+  "#dashboard-content-container",
+);
+dMenuController.addEventListener("click", () => {
+  if (!isCollapsed) {
+    dMenuContainer.classList.add("dashboard-menu-collapsed");
+    dContentContainer.classList.add("dashboard-menu-collapsed");
+  } else {
+    dMenuContainer.classList.remove("dashboard-menu-collapsed");
+    dContentContainer.classList.remove("dashboard-menu-collapsed");
+  }
+
+  isCollapsed = !isCollapsed;
+});
