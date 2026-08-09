bd604ceb75a0e54ac50c6f2b3580b6340391bd39
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-08-09 16:28:42 +0600

    make the university wise filter search for the candidate

--- admin/server/fetch_result_list.php
+++ admin/server/fetch_result_list.php
@@ -11,25 +11,29 @@ $circular_id = $_POST["circular_id"] ?? "";
 // Fetch all
 if ($circular_id == "") {
 
-    $query = "SELECT
-                cgi.*,
-                pc.circular_title
-            FROM candidate_general_information cgi
-            INNER JOIN publish_circular pc
-                ON cgi.circular_id = pc.circular_id
-            WHERE cgi.applicant_status = 3
-            ORDER BY cgi.id DESC";
+    // $query = "SELECT
+    //             cgi.*,
+    //             pc.circular_title
+    //         FROM candidate_general_information cgi
+    //         INNER JOIN publish_circular pc
+    //             ON cgi.circular_id = pc.circular_id
+    //         WHERE cgi.applicant_status = 3
+    //         ORDER BY cgi.id DESC";
+
+    $query = "SELECT cgi.*, pc.circular_title, GROUP_CONCAT( DISTINCT ce.edu_institution ORDER BY ce.id SEPARATOR ' | ' ) AS edu_institutions FROM candidate_general_information cgi INNER JOIN publish_circular pc ON cgi.circular_id = pc.circular_id LEFT JOIN candidate_education ce ON ce.user_id = cgi.user_id AND ce.circular_id = cgi.circular_id WHERE cgi.applicant_status = 3 GROUP BY cgi.id ORDER BY cgi.id DESC";
 } else {
 
-    $query = "SELECT
-                cgi.*,
-                pc.circular_title
-            FROM candidate_general_information cgi
-            INNER JOIN publish_circular pc
-                ON cgi.circular_id = pc.circular_id
-            WHERE cgi.circular_id='$circular_id'
-            AND cgi.applicant_status=3
-            ORDER BY cgi.id DESC";
+    // $query = "SELECT
+    //             cgi.*,
+    //             pc.circular_title
+    //         FROM candidate_general_information cgi
+    //         INNER JOIN publish_circular pc
+    //             ON cgi.circular_id = pc.circular_id
+    //         WHERE cgi.circular_id='$circular_id'
+    //         AND cgi.applicant_status=3
+    //         ORDER BY cgi.id DESC";
+
+    $query = "SELECT cgi.*, pc.circular_title, GROUP_CONCAT( DISTINCT ce.edu_institution ORDER BY ce.id SEPARATOR ' | ' ) AS edu_institutions FROM candidate_general_information cgi INNER JOIN publish_circular pc ON cgi.circular_id = pc.circular_id LEFT JOIN candidate_education ce ON ce.user_id = cgi.user_id AND ce.circular_id = cgi.circular_id WHERE cgi.circular_id = '$circular_id' AND cgi.applicant_status = 3 GROUP BY cgi.id ORDER BY cgi.id DESC";
 }
 
 echo json_encode(
